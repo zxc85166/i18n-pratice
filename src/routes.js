5808feda939e9369603b16560d94880cd7ebfff6
iiiteam11801 <iiiteam11801@gmail.com>
2021-11-03 18:06:51 +0800

    @

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
-import Home from "./views/Home.vue";
-import Soojin from "./views/Soojin.vue";
-import Jiyoon from "./views/Jiyoon.vue";
-import Monday from "./views/Monday.vue";
-import Soeun from "./views/Soeun.vue";
-import Jaehee from "./views/Jaehee.vue";
-import Jihan from "./views/Jihan.vue";
-import Zoa from "./views/Zoa.vue";
-import NotFound from "./views/NotFound.vue";
+import Home from "@/views/Home.vue";
+import Soojin from "@/views/Soojin.vue";
+import Jiyoon from "@/views/Jiyoon.vue";
+import Monday from "@/views/Monday.vue";
+import Soeun from "@/views/Soeun.vue";
+import Jaehee from "@/views/Jaehee.vue";
+import Jihan from "@/views/Jihan.vue";
+import Zoa from "@/views/Zoa.vue";
+import NotFound from "@/views/NotFound.vue";
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-  { path: "/", component: Home, meta: { title: "Home" } },
+  { path: "/", component: Home, meta: { title: "Weeekly" } },
   // component: () => import('./views/Soojin.vue')
   {
     path: "/soojin",
