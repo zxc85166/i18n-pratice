2b35f5174c94bd0b081e7c3bc06dfd38a2f91961
iiiteam11801 <iiiteam11801@gmail.com>
2021-11-04 15:59:34 +0800

    path

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,8 @@ export default defineConfig({
   plugins: [
     vue(),
     vueI18n({
-      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
-      // compositionOnly: false,
-
-      // you need to set i18n resource including paths !
-      include: path.resolve(__dirname, "./path/to/src/locales/**"),
-      // include: path.resolve(__dirname, './src/locales/**'),
+      // include: path.resolve(__dirname, "./path/to/src/locales/**"),
+      include: path.resolve(__dirname, "./src/locales/**"),
     }),
   ],
   resolve: {
